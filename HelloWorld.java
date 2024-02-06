c8e1dcc7d72d0703453eea43d4cbf375c4d28084
Kristina Ledic <kristina.ledic@adcubum.com>
2024-02-06 14:23:15 +0100

    Modification

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
 
    public static void main(String args[])  //static method
    {
-      System.out.println("Hello world");
+      System.out.println("Hello world & welcome to Jenkins training");
    }
 }
